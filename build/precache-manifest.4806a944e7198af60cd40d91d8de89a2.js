c37bdcdb6f60c838a73388506609420b85ea4094
Torben <torben.mannhart@gmail.com>
2020-04-05 13:14:43 +0200

    added requirements

---
+++ build/precache-manifest.4806a944e7198af60cd40d91d8de89a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13600c489d83f077e20aa586f979d19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7eee54421b63d070667",
+    "url": "/static/css/main.c3b755f1.chunk.css"
+  },
+  {
+    "revision": "ed1572d5f514e5e0a42b",
+    "url": "/static/js/2.86a8604c.chunk.js"
+  },
+  {
+    "revision": "5ee7a242877f91e6eba05730c88ffd10",
+    "url": "/static/js/2.86a8604c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7eee54421b63d070667",
+    "url": "/static/js/main.54271a68.chunk.js"
+  },
+  {
+    "revision": "3750558fc2f6b427c64d",
+    "url": "/static/js/runtime-main.7bb9c619.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
